bc06f7f9fc946790ceca29bbd02d1af11e1b75d9
andrademicael <micaelandrade@gmail.com>
2017-12-06 14:13:43 -0300

    adicionado o desvio padrão

--- caracterizacao_variaveis.py
+++ caracterizacao_variaveis.py
@@ -1,6 +1,6 @@
 import sys           
-# sys.path.insert(0, "/home/micael/Dropbox/MESTRADO/Mestrado-Micael-2017-1/Algoritmos/python_modules")
-sys.path.insert(0, "/home/iquanta/Micael/python_modules")
+sys.path.insert(0, "/home/micael/Dropbox/MESTRADO/Mestrado-Micael-2017-1/Algoritmos/python_modules")
+# sys.path.insert(0, "/home/iquanta/Micael/python_modules")
 from numpy import zeros, arange
 from cascadef import strings
 import customplot as ctp 
@@ -10,15 +10,17 @@ import matplotlib.pyplot as plt
 ordem = 6 # ordem da expansão
 snr = arange(11) # valor máximo da snr
 n_re = 1000 # número de realizações das VA's
-n_r = 10000 # numero de repetições do experimento
+n_r = 20 # numero de repetições do experimento
 m_e = zeros((snr.size,ordem))
+std = zeros((snr.size,ordem))
 
 for i in snr:
-	sume = zeros((1,ordem))[0]
+	sume = zeros((n_re,ordem))
 	for t in range(n_r):
-		a, b, err = strings(i, ordem, n_re, display = True)
-		sume += err/n_r # calcula a média de erros e cada repetição do experimento
-	m_e[i,:] = sume/n_re # calcula a porcentagem de erros gerada para cada valor de SNR em cada canal
+		a, b, err = strings(i, ordem, n_re)
+		sume[t,:] = err # calcula a média de erros e cada repetição do experimento
+	m_e[i,:] = sume.mean(0) # calcula a porcentagem de erros gerada para cada valor de SNR em cada canal
+	std[i,:] = sume.std(0, ddof=1)
 
 fig = ctp.newfig(0.8)
 for k in range(ordem):
